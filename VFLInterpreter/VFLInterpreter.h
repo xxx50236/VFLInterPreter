d89941d56b8973a380f749ee3d792d3fe3773785
cb <chenbindeveloper@gmail.com>
2021-01-11 19:16:24 +0800

    first commit

---
+++ VFLInterpreter/VFLInterpreter.h
@@ -0,0 +1,18 @@
+//
+//  VFLInterpreter.h
+//  VFLInterpreter
+//
+//  Created by ChenBin on 2021/1/8.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VFLInterpreter.
+FOUNDATION_EXPORT double VFLInterpreterVersionNumber;
+
+//! Project version string for VFLInterpreter.
+FOUNDATION_EXPORT const unsigned char VFLInterpreterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VFLInterpreter/PublicHeader.h>
+
+
